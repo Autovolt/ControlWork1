e4b6cfa9f9538d389c525aa4b55b07ba82cf45c0
Василий <bryant24@mail.ru>
2022-12-04 20:43:42 +0500

    Creat a code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//   Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна трём символам. Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+// лучше обойтись исключительно массивами.
+// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] -> []
+
+string[] arr = NewArray();
+int len = 0;
+string[] NewArray()
+{
+    Console.Write("Введите значения через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
+void SecondArray(string[] arr, out int len)
+{
+    len = 0;
+    int count = 1;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            len = count;
+            count++;
+        }
+    }
+}
+SecondArray(arr, out len);
+string[] arr2 = new string[len];
+void SecondArrayFind(string[] arr, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            arr2[count] = arr[i];
+            count++;
+        }
+    }
+}
+SecondArrayFind(arr, arr2);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < len; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.WriteLine();
+}
+
+SecondArrayFind(arr, arr2);
+PrintArray(arr2);
